54f2648c700931f6470ab52f6d73ed0494c285eb
App Jam <appjam@dhcp-v039-222.mobile.uci.edu>
2013-11-14 14:13:34 -0800

    test
    test

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by App Jam on 11/12/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
